1c996f5f297c8e41b38e583199b52903769c8cbe
YuShangYong <1130221300@qq.com>
2023-01-28 20:58:25 +0800

    Initial commit

---
+++ src/basic/入门代码/Basic001.java
@@ -0,0 +1,16 @@
+package basic.»Î√≈¥˙¬Î;
+
+public class Basic001 {
+
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+
+//        f.setIconImage(new ImageIcon("img\\component\\female.png").getImage());
+//      f.setIconImage(Toolkit.getDefaultToolkit().getImage("img\\component\\female.png"));
+
+
+        long end = System.currentTimeMillis();
+        System.out.printf("total:"+(end-start)/1000.00+"S");
+    }
+}
