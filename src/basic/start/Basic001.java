83264e1a93cd1768547918e3f8312e34c5dd52bc
YuShangYong <1130221300@qq.com>
2023-01-28 21:26:26 +0800

    Yu Commit

--- src/basic/start/Basic001.java
+++ src/basic/start/Basic001.java
@@ -8,9 +8,12 @@ public class Basic001 {
 
 //        f.setIconImage(new ImageIcon("img\\component\\female.png").getImage());
 //      f.setIconImage(Toolkit.getDefaultToolkit().getImage("img\\component\\female.png"));
-
+        int j=0;
+        for(int i=1;i<=1000000;i++){
+            j=i;
+        }
 
         long end = System.currentTimeMillis();
-        System.out.printf("total:"+(end-start)/1000.00+"S");
+        System.out.println("total:"+j+" ºÄÊ±£º"+(end-start) +"ºÁÃë");
     }
 }
